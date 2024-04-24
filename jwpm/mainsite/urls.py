1981b083da941d6110c0b2852865e548602c1619
dwrigley1 <dwrigley1@ivytech.edu>
2024-04-24 04:39:23 -0400

    Updated HTML & weblinks
    1.) Created the remaining four HTML documents needed to reflect the given links: Rental Application, Contact Us, Rental Requirements, Available Units. I did not change any presentation effects with these, simply created them & edited the title so that I can point the backend to something.
    2.) Added 4 new paths in urls.py
    3.) defined 4 new functions in views.py
    4.) edited the lines (17-21?) in main.html to allow the links to redirect the user to the newly added pages.

--- jwpm/mainsite/urls.py
+++ jwpm/mainsite/urls.py
@@ -2,6 +2,16 @@ from django.urls import path
 
 from . import views
 
+''' original code on lines 6, 7, 8, in case it is ever needed
 urlpatterns = [
     path("", views.main, name="main"),
+] 
+'''
+
+urlpatterns = [
+    path('', views.main, name='main'),
+    path('rental_requirements/', views.rental_requirements, name='rental_requirements'),
+    path('available_units/', views.available_units, name='available_units'),
+    path('rental_application/', views.rental_application, name='rental_application'),
+    path('contact_us/', views.contact_us, name='contact_us'),
 ]
--- jwpm/mainsite/views.py
+++ jwpm/mainsite/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
 def main(request):
-    return render(request, 'main.html', {})
+    return render(request, 'main.html')
+
+def rental_requirements(request):
+    return render(request, 'rental_requirements.html')
+
+def available_units(request):
+    return render(request, 'available_units.html')
+
+def rental_application(request):
+    return render(request, 'rental_application.html')
+
+def contact_us(request):
+    return render(request, 'contact_us.html')
