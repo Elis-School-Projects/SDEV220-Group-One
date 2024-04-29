4f9b8f1d009591dd731614666f85abe93009c4f2
Eli Walters <ewalters37@ivytech.edu>
2024-04-28 23:59:28 -0400

    Update: Navigation and All Non-Main Pages

---
+++ jwpm/mainsite/static/js/main.js
@@ -0,0 +1,7 @@
+function openNavigation() {
+    document.querySelector('.header__popoutNav').style.display = "block";
+}
+
+function closeNavigation() {
+    document.querySelector('.header__popoutNav').style.display = "none";
+}
