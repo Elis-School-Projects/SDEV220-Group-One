c42d309abf5fb6ecdaf684820c7f00f03f669ce5
Eli Walters <ewalters37@ivytech.edu>
2024-05-04 10:22:18 -0400

    Create DB Models

--- jwpm/mainsite/models.py
+++ jwpm/mainsite/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
-# Create your models here.
+class Unit(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    address = models.CharField(max_length=200)
+    status = models.TextField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.FloatField()
+    rent = models.IntegerField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def remove(self):
+        self.published_date = None
+        self.save()
+
+    def __str__(self):
+        return self.address
+    
+class Unit(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.TextField()
+    phone = models.TextField()
+    income = models.IntegerField()
+    applyingFor = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
--- jwpm/mainsite/urls.py
+++ jwpm/mainsite/urls.py
@@ -2,12 +2,6 @@ from django.urls import path
 
 from . import views
 
-''' original code on lines 6, 7, 8, in case it is ever needed
-urlpatterns = [
-    path("", views.main, name="main"),
-] 
-'''
-
 urlpatterns = [
     path('', views.main, name='main'),
     path('rental_requirements/', views.rental_requirements, name='rental_requirements'),
